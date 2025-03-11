5778e3831be277e0dab61518fc01c1db23522fcc
Omer Šabić <omer@datalinx.si>
2025-03-11 14:30:40 +0100

    fix: restore role site relation

--- src/Models/User/Role.php
+++ src/Models/User/Role.php
@@ -3,12 +3,18 @@
 namespace Eclipse\Core\Models\User;
 
 use Eclipse\Core\Database\Factories\RoleFactory;
+use Eclipse\Core\Models\Site;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
     use HasFactory;
+
+    public function site()
+    {
+        return $this->belongsTo(Site::class);
+    }
 
     protected static function newFactory()
     {
