de8ce4cfebbc33c37eb9a2ad9d8209ed64269710
Omer Šabić <omer@datalinx.si>
2025-06-17 11:30:36 +0200

    test: add more user tests

--- tests/Feature/Filament/Resources/RoleResourceTest.php
+++ tests/Feature/Filament/Resources/RoleResourceTest.php
@@ -3,19 +3,14 @@
 use Eclipse\Core\Filament\Resources\RoleResource\Pages\CreateRole;
 use Eclipse\Core\Filament\Resources\RoleResource\Pages\EditRole;
 use Eclipse\Core\Models\Site;
-use Eclipse\Core\Models\User;
 use Eclipse\Core\Models\User\Role;
 use Filament\Facades\Filament;
 
 use function Pest\Livewire\livewire;
 
 test('role can be created with site assignment', function () {
-    $site = Site::factory()->create();
-    $admin = User::factory()->create();
-    $admin->assignRole('super_admin');
-
-    $this->actingAs($admin);
-    Filament::setTenant($site);
+    $this->set_up_super_admin_and_tenant();
+    $site = Filament::getTenant();
 
     livewire(CreateRole::class, ['tenant' => $site])
         ->fillForm([
@@ -32,15 +27,11 @@ test('role can be created with site assignment', function () {
 });
 
 test('role can be edited to change site assignment', function () {
-    $site1 = Site::factory()->create();
+    $this->set_up_super_admin_and_tenant();
+    $site1 = Filament::getTenant();
+
     $site2 = Site::factory()->create();
     $role = Role::factory()->create(['site_id' => $site1->id]);
-
-    $admin = User::factory()->create();
-    $admin->assignRole('super_admin');
-
-    $this->actingAs($admin);
-    Filament::setTenant($site1);
 
     livewire(EditRole::class, ['record' => $role->id, 'tenant' => $site1])
         ->fillForm([
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,8 @@
 
 use Eclipse\Core\Models\Site;
 use Eclipse\Core\Models\User;
+use Filament\Facades\Filament;
+use Spatie\Permission\Models\Role;
 
 test('new user automatically gets panel_user role', function () {
     $user = User::factory()->create();
@@ -21,3 +23,69 @@ test('user can only access sites they belong to', function () {
     expect($user->canAccessTenant($site1))->toBeTrue();
     expect($user->canAccessTenant($site2))->toBeFalse();
 });
+
+test('user is correctly given global role', function () {
+    Role::create(['name' => 'global_admin', 'guard_name' => 'web']);
+
+    $user = User::factory()->create();
+    $user->assignRole('global_admin');
+    $this->actingAs($user);
+
+    expect($user->hasRole('global_admin'))->toBeTrue();
+
+    $site = Site::factory()->create();
+    Filament::setTenant($site);
+
+    expect($user->hasRole('global_admin'))->toBeTrue();
+
+    $site2 = Site::factory()->create();
+    Filament::setTenant($site2);
+
+    expect($user->hasRole('global_admin'))->toBeTrue();
+});
+
+test('user is correctly given site role', function () {
+    $site = Site::factory()->create();
+    $siteRole = Role::create(['name' => 'site_admin', 'guard_name' => 'web', 'site_id' => $site->id]);
+
+    $user = User::factory()->create();
+    $user->assignRole($siteRole);
+    $this->actingAs($user);
+
+    Filament::setTenant($site);
+
+    expect($user->hasRole('site_admin'))->toBeTrue();
+});
+
+test('users with global roles are added to new sites', function () {
+    $globalRole = Role::create(['name' => 'global_admin', 'guard_name' => 'web']);
+    $user = User::factory()->create();
+    $user->assignRole($globalRole);
+    $this->actingAs($user);
+
+    $site = Site::factory()->create();
+    Filament::setTenant($site);
+
+    expect($user->hasRole('global_admin'))->toBeTrue();
+});
+
+test('users with site roles are not added to new sites', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $site = Site::factory()->create();
+    $siteRole = Role::create(['name' => 'site_admin', 'guard_name' => 'web', 'site_id' => $site->id]);
+    Filament::setTenant($site);
+
+    $user->assignRole($siteRole);
+
+    // Check on first site
+    expect($user->hasRole('site_admin'))->toBeTrue();
+
+    // Create extra site and re-check
+    $site2 = Site::factory()->create();
+    Filament::setTenant($site2);
+
+    expect($user->hasRole('site_admin'))->toBeFalse();
+});
+
