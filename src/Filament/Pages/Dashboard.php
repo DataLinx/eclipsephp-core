fab4376e36af206f9c7781237e16a3f2c7526417
Thapa Godar Ank. <76224530+thapacodes4u@users.noreply.github.com>
2025-07-08 07:13:21 -0700

    feat: improve main nav menu
    Co-authored-by: ankitcodes4u <ankitcodes4u@gmail.com>

---
+++ src/Filament/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Eclipse\Core\Filament\Pages;
+
+use Filament\Pages\Dashboard as BasePage;
+
+class Dashboard extends BasePage
+{
+    protected static bool $shouldRegisterNavigation = false;
+}
