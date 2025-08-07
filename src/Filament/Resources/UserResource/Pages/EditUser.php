82759fdb3cb5e05a2f24932a571ceaf4767c3086
Kilian Trunk <hunterteammovies@gmail.com>
2025-08-07 16:58:20 +0200

    feat(record-navigation): implement user navigation

--- src/Filament/Resources/UserResource/Pages/EditUser.php
+++ src/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,9 +5,14 @@ namespace Eclipse\Core\Filament\Resources\UserResource\Pages;
 use Eclipse\Core\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Nben\FilamentRecordNav\Actions\NextRecordAction;
+use Nben\FilamentRecordNav\Actions\PreviousRecordAction;
+use Nben\FilamentRecordNav\Concerns\WithRecordNavigation;
 
 class EditUser extends EditRecord
 {
+    use WithRecordNavigation;
+
     protected static string $resource = UserResource::class;
 
     public function hasCombinedRelationManagerTabsWithContent(): bool
@@ -23,6 +28,8 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            PreviousRecordAction::make(),
+            NextRecordAction::make(),
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
