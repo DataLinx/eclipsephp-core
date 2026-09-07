a58f2241d97c69645ac4ecee7154104389d0136c
SlimDeluxe <131700+SlimDeluxe@users.noreply.github.com>
2026-09-07 09:29:22 +0000

    style: fix code style

--- src/EclipseServiceProvider.php
+++ src/EclipseServiceProvider.php
@@ -13,7 +13,6 @@ use Eclipse\Core\Console\Commands\SetupReverb;
 use Eclipse\Core\Health\Checks\ReverbCheck;
 use Eclipse\Core\Listeners\LogEmailToDatabase;
 use Eclipse\Core\Listeners\SendEmailSuccessNotification;
-use Eclipse\Core\Models\Locale;
 use Eclipse\Core\Models\User;
 use Eclipse\Core\Notifications\Channels\SiteDatabaseChannel;
 use Eclipse\Core\Providers\AdminPanelProvider;
