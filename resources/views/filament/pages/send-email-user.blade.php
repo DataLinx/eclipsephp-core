72ebff6772b637a7063d22068b11599a650c7fdb
Omer Šabić <omer@datalinx.si>
2026-09-08 10:17:57 +0200

    fix: upgrade template to Filament v5

--- resources/views/filament/pages/send-email-user.blade.php
+++ resources/views/filament/pages/send-email-user.blade.php
@@ -1,9 +1,11 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="send">
+    <form wire:submit="send">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
-    </x-filament-panels::form>
-</x-filament-panels::page> 
+        <div class="mt-6">
+            <x-filament::actions
+                :actions="$this->getFormActions()"
+            />
+        </div>
+    </form>
+</x-filament-panels::page>
